3f74ca297bf769909ecb4f8d32b31fe02df039c5
Daragh <daraghcoll-@hotmail.com>
2023-10-20 00:50:16 +0700

    feat: custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-primary': '#131315',
+        'dark-secondary': '#181A1D',
+      },
+    },
   },
   plugins: [],
 };
