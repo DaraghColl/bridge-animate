3eb8ca872f402a025fb718a10ebb7283ba74f430
Daragh <daraghcoll-@hotmail.com>
2023-10-20 02:19:14 +0700

    feat: state for selected element

--- src/components/element-list/element-list.tsx
+++ src/components/element-list/element-list.tsx
@@ -1,5 +1,6 @@
 import { FC, useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { useSelectedElementContext } from '../../state/selected-element';
 
 const setElementIds = (elements: Element[]): Element[] => {
   const elementsWithIds = elements.map((element) => {
@@ -16,6 +17,11 @@ const setElementIds = (elements: Element[]): Element[] => {
 
 const ElementList: FC = () => {
   const [elements, setElements] = useState<Element[] | null>(null);
+  const { setSelectedElementId } = useSelectedElementContext();
+
+  const handleElementSelect = (element: Element) => {
+    setSelectedElementId(element.getAttribute('id'));
+  };
 
   useEffect(() => {
     const canvas = document.getElementById('canvas');
@@ -37,7 +43,7 @@ const ElementList: FC = () => {
       {elements &&
         elements.map((element, index) => {
           return (
-            <div key={index} className="px-4 py-2">
+            <div key={index} className="px-4 py-2" onClick={() => handleElementSelect(element)}>
               {element.getAttribute('id') ?? element.tagName}
             </div>
           );
---
+++ src/state/selected-element.tsx
@@ -0,0 +1,42 @@
+import { Dispatch, FC, ReactNode, SetStateAction, createContext, useContext, useEffect, useState } from 'react';
+
+interface SelectedElementValue {
+  selectedElementID: string | null;
+  setSelectedElementId: Dispatch<SetStateAction<string | null>>;
+}
+
+const SelectedElementContext = createContext<SelectedElementValue | undefined>(undefined);
+
+interface SelectedElementProviderProps {
+  children: ReactNode;
+}
+
+const SelectedElementProvider: FC<SelectedElementProviderProps> = ({ children }) => {
+  const [selectedElementID, setSelectedElementId] = useState<string | null>(null);
+  const [previousSelectedElementID, setPreviousSelectedElementId] = useState<string>('');
+
+  useEffect(() => {
+    if (selectedElementID !== null) {
+      document.getElementById(previousSelectedElementID)?.classList.remove('outline', 'outline-blue-500');
+      document.getElementById(selectedElementID)?.classList.add('outline', 'outline-blue-500');
+      setPreviousSelectedElementId(selectedElementID);
+    }
+  }, [selectedElementID, previousSelectedElementID]);
+
+  return (
+    <SelectedElementContext.Provider value={{ selectedElementID, setSelectedElementId }}>
+      {children}
+    </SelectedElementContext.Provider>
+  );
+};
+
+const useSelectedElementContext = () => {
+  const selectedElementContext = useContext(SelectedElementContext);
+  if (selectedElementContext === undefined) {
+    throw new Error('useSelectedElementContext must be inside a SelectedElementProvider');
+  }
+  return selectedElementContext;
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export { SelectedElementProvider, useSelectedElementContext };
