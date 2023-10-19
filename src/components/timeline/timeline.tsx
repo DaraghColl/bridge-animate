906a5acfcec5788925dd4a9d3388bca360842017
Daragh <daraghcoll-@hotmail.com>
2023-10-20 00:51:01 +0700

    feat: timeline colors and change grid to flex

--- src/components/timeline/timeline.tsx
+++ src/components/timeline/timeline.tsx
@@ -4,11 +4,11 @@ import { Keyframes } from './keyframes';
 
 const Timeline: FC = () => {
   return (
-    <div className="grid h-1/4 grid-cols-[20%_80%]">
-      <div className="border-2 border-indigo-800 p-2">
+    <div className=" flex h-1/4 gap-4 rounded-md">
+      <div className="basis-1/4 overflow-scroll bg-dark-secondary p-2">
         <Layers />
       </div>
-      <div className="border-2 border-indigo-800 p-4">
+      <div className="basis-3/4 overflow-scroll bg-dark-secondary p-4">
         <Keyframes />
       </div>
     </div>
