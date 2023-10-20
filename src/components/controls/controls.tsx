7b4dc0f546b690fda6382a87aebfb9e58c98a516
Daragh <daraghcoll-@hotmail.com>
2023-10-20 22:23:50 +0700

    feat: group controls

--- src/components/controls/controls.tsx
+++ src/components/controls/controls.tsx
@@ -13,40 +13,57 @@ const Controls: FC = () => {
   };
 
   return (
-    <div>
-      <div>
-        <label htmlFor="opacity">opacity</label>
-        <input name="opacity" type="number" className="text-black" onChange={(e) => handleInputChange('opacity', e)} />
-      </div>
-
-      <div>
-        <label htmlFor="xPosition">x position</label>
+    <div className="flex flex-col gap-4 p-4">
+      <div className="flex flex-col">
+        <label htmlFor="opacity" className="mb-2 text-gray-100">
+          opacity
+        </label>
         <input
-          name="xPosition"
+          name="opacity"
           type="number"
-          className="text-black"
-          onChange={(e) => handleInputChange('translateX', e)}
+          className="rounded-sm bg-dark-primary px-2 py-1 text-gray-100 outline-none"
+          onChange={(e) => handleInputChange('opacity', e)}
         />
       </div>
 
-      <div>
-        <label htmlFor="yPosition">y position</label>
-        <input
-          name="yPosition"
-          type="number"
-          className="text-black"
-          onChange={(e) => handleInputChange('translateY', e)}
-        />
+      <div className="mb-2 flex flex-col">
+        <span>Position</span>
+        <div className="mt-2 flex flex-row gap-2">
+          <div className="basis-1/2">
+            <label htmlFor="xPosition" className="ml-2 text-indigo-500">
+              x
+            </label>
+            <input
+              name="xPosition"
+              type="number"
+              className="w-full rounded-sm bg-dark-primary px-2 py-1 text-gray-100 outline-none"
+              onChange={(e) => handleInputChange('translateX', e)}
+            />
+          </div>
+          <div className="basis-1/2">
+            <label htmlFor="yPosition" className="ml-2 text-indigo-500">
+              y
+            </label>
+            <input
+              name="yPosition"
+              type="number"
+              className="w-full rounded-sm bg-dark-primary px-2 py-1 text-gray-100 outline-none"
+              onChange={(e) => handleInputChange('translateY', e)}
+            />
+          </div>
+        </div>
       </div>
 
-      <div>
-        <label htmlFor="rotate">rotate</label>
+      <div className="flex flex-col">
+        <label htmlFor="rotate" className="mb-2 text-gray-100">
+          rotate
+        </label>
         <input
           name="rotate"
           type="number"
           // min={0}
           // max={360}
-          className="text-black"
+          className="rounded-sm bg-dark-primary px-2 py-1 text-gray-100 outline-none"
           onChange={(e) => handleInputChange('rotate', e)}
         />
       </div>
