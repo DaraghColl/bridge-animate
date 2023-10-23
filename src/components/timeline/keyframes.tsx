9c198fa36394634fd1ced069cd047d5b6e313f20
Daragh <daraghcoll-@hotmail.com>
2023-10-24 01:37:44 +0700

    feat: move play animation functionality to timeline component

--- src/components/timeline/keyframes.tsx
+++ src/components/timeline/keyframes.tsx
@@ -2,68 +2,18 @@ import { FC, Fragment } from 'react';
 import { useSelectedElementContext } from '../../state/selected-element';
 import { KeyframeTime, useAnimationsContext } from '../../state/animations';
 
-const Keyframes: FC = () => {
-  return (
-    <div className="flex flex-col items-center justify-around">
-      <Keyframe />
-    </div>
-  );
-};
-
 const keyframes: string[] = ['0', '0.25', '0.50', '0.75', '1'];
+
+// interface KeyframeProps {
+//   handlePlayAnimation: () => void;
+// }
 
 const Keyframe: FC = () => {
   const { selectedElementID } = useSelectedElementContext();
   const { animations, createKeyframe, selectedKeyFrameTime } = useAnimationsContext();
 
-  const handlePlayAnimation = () => {
-    if (animations) {
-      // loop through all elements with animations
-      animations.forEach((animation) => {
-        const elementToAnimate = document.getElementById(animation.name);
-        const javascriptFormattedAnimations: Keyframe[] = [];
-
-        animation.keyframes.forEach(({ styles, time }) => {
-          for (const [key, value] of Object.entries(styles)) {
-            if (value !== null && value !== '') {
-              if (key === 'translateX' || key === 'translateY') {
-                javascriptFormattedAnimations.push({
-                  transform: `${key}(${value}px)`,
-                  offset: Number(time),
-                });
-              } else if (key === 'rotate') {
-                javascriptFormattedAnimations.push({
-                  transform: `${key}(${value}deg)`,
-                  offset: Number(time),
-                });
-              } else {
-                javascriptFormattedAnimations.push({
-                  [key]: value,
-                  offset: Number(time),
-                });
-              }
-            }
-          }
-        });
-
-        // join all transforms as js animate groups all transforms - {transform: 'rotate translate...'}
-        const joinedTransforms = javascriptFormattedAnimations.map((jt) => jt.transform).join(' ');
-        const animsWithoutTransforms = javascriptFormattedAnimations.filter((awt) => !awt.transform);
-        const joinedAnims = [...animsWithoutTransforms, { transform: joinedTransforms }];
-
-        elementToAnimate?.animate(joinedAnims, {
-          // timing options
-          duration: 1500,
-          fill: 'none',
-          easing: 'ease-in-out',
-          iterations: 1,
-        });
-      });
-    }
-  };
-
   return (
-    <Fragment>
+    <div className="flex flex-col items-center justify-around">
       {selectedElementID && animations && animations?.length > 0 && animations !== null && (
         <div className="relatice bg-white-400 flex h-2 w-full items-center justify-between rounded-sm bg-white">
           {keyframes.map((keyframe, index) => {
@@ -82,16 +32,8 @@ const Keyframe: FC = () => {
           })}
         </div>
       )}
-      <div className="absolute bottom-5 right-5 mt-10 flex gap-4">
-        <button
-          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-          onClick={handlePlayAnimation}
-        >
-          Play Animation
-        </button>
-      </div>
-    </Fragment>
+    </div>
   );
 };
 
-export { Keyframes };
+export { Keyframe };
--- src/components/timeline/timeline.tsx
+++ src/components/timeline/timeline.tsx
@@ -1,15 +1,97 @@
 import { FC } from 'react';
 import { Layers } from './layers';
-import { Keyframes } from './keyframes';
+import { Keyframe } from './keyframes';
+import { useAnimationsContext } from '../../state/animations';
+
+interface StyleObjectKeys {
+  [key: string]: string | number | null | undefined;
+}
+interface StyleObject extends StyleObjectKeys {
+  opacity?: string | null;
+  rotate?: string | null;
+  translateX?: string | null;
+  translateY?: string | null;
+  offset: number;
+}
+
+interface FormattedStyleObject extends StyleObjectKeys {
+  opacity?: string | null;
+  transform?: string | null;
+  offset: number;
+}
 
 const Timeline: FC = () => {
+  const { animations } = useAnimationsContext();
+
+  const handlePlayAnimation = () => {
+    if (animations) {
+      // loop through all elements with animations
+      animations.forEach((animation) => {
+        const elementToAnimate = document.getElementById(animation.name);
+        // const javascriptFormattedAnimations: Keyframe[] = [];
+        const javascriptFormattedAnimations: FormattedStyleObject[] = [];
+
+        animation.keyframes.forEach(({ styles, time }) => {
+          const animationStyleObject: StyleObject = {
+            opacity: null,
+            rotate: null,
+            translateX: null,
+            translateY: null,
+            offset: Number(time),
+          };
+
+          for (const [key, value] of Object.entries(styles)) {
+            if (value !== null && value !== '') {
+              if (key === 'translateX' || key === 'translateY') {
+                animationStyleObject[key] = `${key}(${value}px)`;
+              } else if (key === 'rotate') {
+                animationStyleObject[key] = `${key}(${value}deg)`;
+              } else {
+                animationStyleObject[key] = value;
+              }
+            }
+          }
+          javascriptFormattedAnimations.push(animationStyleObject);
+        });
+
+        const formatTransform = javascriptFormattedAnimations.map(
+          ({ opacity, rotate, translateX, translateY, offset }) => {
+            return {
+              opacity: opacity ? opacity : '1',
+              transform: `${rotate ? rotate : ''} ${translateX ? translateX : ''} ${translateY ? translateY : ''}`,
+              offset: offset,
+            };
+          },
+        );
+
+        elementToAnimate?.animate(formatTransform, {
+          duration: 1500,
+          fill: 'auto',
+          easing: 'ease-in-out',
+          iterations: 1,
+        });
+      });
+    }
+  };
+
   return (
     <div className=" flex h-1/4 gap-4 rounded-md">
       <div className="basis-1/4 overflow-scroll bg-dark-secondary p-2">
         <Layers />
       </div>
-      <div className="basis-3/4 overflow-scroll bg-dark-secondary p-4">
-        <Keyframes />
+      <div className="flex basis-3/4 flex-col gap-8 overflow-scroll bg-dark-secondary p-4">
+        {/* {animations?.map(() => {
+          return <Keyframe />;
+        })} */}
+        <Keyframe />
+        <div className="absolute bottom-5 right-5 mt-10 flex gap-4">
+          <button
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+            onClick={handlePlayAnimation}
+          >
+            Play Animation
+          </button>
+        </div>
       </div>
     </div>
   );
