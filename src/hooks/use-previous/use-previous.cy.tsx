5da856d366874c360fa8e42df3cbada3ee134ddd
Daragh <daraghcoll-@hotmail.com>
2023-11-05 12:33:03 +0700

    feat: unit test for use previous hook

---
+++ src/hooks/use-previous/use-previous.cy.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable react-refresh/only-export-components */
+import { useState } from 'react';
+import { usePrevious } from './use-previous';
+
+const MockUsePreviousComponent = () => {
+  const [count, setCount] = useState<number>(1);
+  const previousCount = usePrevious(count);
+  console.log(count);
+  console.log(previousCount);
+
+  const handleClick = () => {
+    setCount((prevCount: number) => prevCount + 1);
+  };
+
+  return (
+    <div>
+      <button data-cy="mock_increase_count" onClick={handleClick}>
+        Click
+      </button>
+      <div data-cy="mock_count">{count}</div>
+      <div data-cy="mock_previous_count">{previousCount}</div>
+    </div>
+  );
+};
+
+describe('<ElementList />', () => {
+  it('should show no element in canvas message', () => {
+    cy.mount(<MockUsePreviousComponent />);
+
+    cy.get('[data-cy="mock_increase_count"]').click();
+    cy.get('[data-cy="mock_count"]').contains(2);
+    cy.get('[data-cy="mock_previous_count"]').contains(1);
+  });
+});
