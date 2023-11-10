5647a964383ce73c8108e4c26c08588fa9c8ce26
Daragh <daraghcoll-@hotmail.com>
2023-11-11 04:32:26 +0800

    fix: style controls container increasing in size when showing controls

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { Layers } from './components/layers/layers';
 function App() {
   return (
     <main className="flex h-screen flex-col gap-4 bg-slate-100 p-4 text-black dark:bg-dark-primary dark:text-white">
-      <div className="flex h-2/3 flex-row gap-4">
+      <div className="flex h-2/3 min-h-0 flex-row gap-4">
         <div className="basis-1/5 overflow-scroll rounded-md bg-white dark:bg-dark-secondary">
           <ElementList />
         </div>
@@ -20,7 +20,7 @@ function App() {
           <Controls />
         </div>
       </div>
-      <div className="flex h-1/3 min-h-0 gap-4 rounded-md">
+      <div className="flex h-1/3 gap-4 rounded-md">
         <div className="basis-1/4 overflow-scroll bg-white p-2 text-black dark:bg-dark-secondary dark:text-white">
           <Layers />
         </div>
