48e0002c28ac005a94708734a54e65dd49ace448
Daragh <daraghcoll-@hotmail.com>
2023-11-18 23:49:42 +0800

    fix: overflow on play controls div

--- src/components/timeline/timeline.tsx
+++ src/components/timeline/timeline.tsx
@@ -108,7 +108,6 @@ const Timeline: FC = () => {
     if (!selectedElementID) return;
 
     if (isPlaying) {
-      console.log('trigge');
       document.getElementById(selectedElementID)?.classList.remove('outline', 'outline-accent');
     } else if (!isPlaying) {
       document.getElementById(selectedElementID)?.classList.add('outline', 'outline-accent');
@@ -130,81 +129,85 @@ const Timeline: FC = () => {
       )}
       {animations && animations.length > 0 && (
         <Fragment>
-          <div className="flex items-center justify-between">
-            {keyframeTimes.map((time) => (
-              <div key={time}>{keyframePercentageMap[time]}</div>
-            ))}
-          </div>
-          <div className="mb-5 flex items-center">
-            <input
-              ref={scrubberRef}
-              value={scrubberValue}
-              id="scrubber"
-              type="range"
-              className="h-1 w-full cursor-pointer appearance-none rounded-lg bg-light-secondary accent-accent"
-              onChange={(e: ChangeEvent<HTMLInputElement>) => onScrubChange(e)}
-              min="0"
-              max="2500"
-              disabled={animationsToPay.length <= 0}
-            ></input>
-          </div>
-          <Keyframe />
+          <div className="flex h-full flex-col justify-between">
+            <div className="flex flex-col gap-4">
+              <div className="flex items-center justify-between">
+                {keyframeTimes.map((time) => (
+                  <div key={time}>{keyframePercentageMap[time]}</div>
+                ))}
+              </div>
+              <div className="mb-5 flex items-center">
+                <input
+                  ref={scrubberRef}
+                  value={scrubberValue}
+                  id="scrubber"
+                  type="range"
+                  className="h-1 w-full cursor-pointer appearance-none rounded-lg bg-light-secondary accent-accent"
+                  onChange={(e: ChangeEvent<HTMLInputElement>) => onScrubChange(e)}
+                  min="0"
+                  max="2500"
+                  disabled={animationsToPay.length <= 0}
+                ></input>
+              </div>
+              <Keyframe />
+            </div>
 
-          <div className="absolute bottom-2 flex w-full justify-center">
-            <div className="right-2 flex basis-1/4 items-center gap-4">
-              <button
-                className="rounded-md bg-accent px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-accent focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent"
-                onClick={handleStopAnimation}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="h-5 w-5"
+            <div className="flex w-full justify-center">
+              <div className="right-2 flex basis-1/4 items-center gap-4">
+                <button
+                  className="rounded-md bg-accent px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-accent focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent"
+                  onClick={handleStopAnimation}
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z"
-                  />
-                </svg>
-              </button>
-              <button
-                className="rounded-md bg-accent px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-accent focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent"
-                onClick={handlePlayAnimation}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="h-5 w-5"
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="h-5 w-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z"
+                    />
+                  </svg>
+                </button>
+                <button
+                  className="rounded-md bg-accent px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-accent focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent"
+                  onClick={handlePlayAnimation}
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
-                  />
-                </svg>
-              </button>
-              <button
-                className="rounded-md bg-accent px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-accent focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent"
-                onClick={handlePauseAnimation}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="h-5 w-5"
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="h-5 w-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+                    />
+                  </svg>
+                </button>
+                <button
+                  className="rounded-md bg-accent px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-accent focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent"
+                  onClick={handlePauseAnimation}
                 >
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
-                </svg>
-              </button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="h-5 w-5"
+                  >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
+                  </svg>
+                </button>
+              </div>
             </div>
           </div>
         </Fragment>
