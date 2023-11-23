2820a5e57077f74ab4a11fcb5ca802105a8681e2
Daragh <daraghcoll-@hotmail.com>
2023-11-23 17:58:46 +0800

    feat: remove animations config and add title icon

--- src/components/layers/layers.tsx
+++ src/components/layers/layers.tsx
@@ -1,6 +1,5 @@
 import { FC, useState } from 'react';
-import { Disclosure } from '@headlessui/react';
-import { ChevronRightIcon, TrashIcon } from '@heroicons/react/24/solid';
+import { TrashIcon, Square3Stack3DIcon } from '@heroicons/react/24/solid';
 import { useAnimationsContext } from '@state/animations';
 import { useSelectedElementContext } from '@state/selected-element';
 import { ConfirmDialog } from '../confirm-dialog/confirm-dialog';
@@ -37,9 +36,12 @@ const Layers: FC = () => {
   };
 
   return (
-    <div className="text-sm font-normal">
+    <div className="p-2 text-sm  font-normal">
       <div className="mb-2 flex items-center justify-between ">
-        <span>Layers</span>
+        <div className="flex items-center gap-2">
+          <Square3Stack3DIcon className="h-4 w-4" />
+          <span>Layers</span>
+        </div>
         <button aria-label="add layer" onClick={handleCreateNewAnimation}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -56,45 +58,30 @@ const Layers: FC = () => {
 
       <div className="flex flex-col items-start gap-4 font-thin">
         {animations &&
-          animations.map(({ id, name, config }, index) => {
+          animations.map(({ id, name }, index) => {
             return (
               <div key={index} className="w-full">
-                <Disclosure>
-                  <div>
-                    <div className="flex w-full cursor-pointer items-center gap-2">
-                      <Disclosure.Button>
-                        <ChevronRightIcon className="w-4 text-slate-medium ui-open:rotate-90 ui-open:transform" />
-                      </Disclosure.Button>
-                      <span
-                        className={`${selectedElementID === name ? 'text-accent' : ''}`}
-                        onClick={() => elementSelect(name)}
-                      >
-                        {name}
-                      </span>
-                      <span>
-                        <TrashIcon
-                          className="w-4 text-dark-primary transition ease-in-out hover:-translate-y-[2px] dark:text-white"
-                          onClick={() => setShowDeleteAnimationLayer(true)}
-                        />
-                        <ConfirmDialog
-                          openDialog={showDeleteAnimationLayer}
-                          onDialogClose={setShowDeleteAnimationLayer}
-                          title="Delete Animation Layer"
-                          message="Are you sure you want to delete this animation layer?"
-                          confirmCallback={() => deleteAnimationLayer(id)}
-                        />
-                      </span>
-                    </div>
-                  </div>
-                  <Disclosure.Panel className="ml-6 flex flex-col pt-2 text-slate-medium">
-                    <span>duration: {config.animationDuration}</span>
-                    <span>delay: {config.animationDelay}</span>
-                    <span>iteration: {config.animationInterationCount}</span>
-                    <span>direction {config.animationDirection}</span>
-                    <span>timing function: {config.animationTimingFunction}</span>
-                    <span>fill mode: {config.animationFillMode}</span>
-                  </Disclosure.Panel>
-                </Disclosure>
+                <div className="flex w-full cursor-pointer items-center gap-2">
+                  <span
+                    className={`${selectedElementID === name ? 'text-accent' : ''}`}
+                    onClick={() => elementSelect(name)}
+                  >
+                    {name}
+                  </span>
+                  <span>
+                    <TrashIcon
+                      className="w-4 text-dark-primary transition ease-in-out hover:-translate-y-[2px] dark:text-white"
+                      onClick={() => setShowDeleteAnimationLayer(true)}
+                    />
+                    <ConfirmDialog
+                      openDialog={showDeleteAnimationLayer}
+                      onDialogClose={setShowDeleteAnimationLayer}
+                      title="Delete Animation Layer"
+                      message="Are you sure you want to delete this animation layer?"
+                      confirmCallback={() => deleteAnimationLayer(id)}
+                    />
+                  </span>
+                </div>
               </div>
             );
           })}
