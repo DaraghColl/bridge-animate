428adee08456fb243df83420cc28c2ac8aa54ed4
Daragh <daraghcoll-@hotmail.com>
2023-11-27 15:06:17 +0800

    feat: add react router and setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { SelectedElementProvider } from '@state/selected-element.tsx';
 import { AnimationsProvider } from '@state/animations.tsx';
 import { CanvasProvider } from '@state/canvas.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <SelectedElementProvider>
       <AnimationsProvider>
         <CanvasProvider>
-          <App />
+          <RouterProvider router={router} />
         </CanvasProvider>
       </AnimationsProvider>
     </SelectedElementProvider>
