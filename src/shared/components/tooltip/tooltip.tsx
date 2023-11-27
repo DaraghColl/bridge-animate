384b55a85d9d95165773d3ad6f9aeb6c7c7910c4
Daragh <daraghcoll-@hotmail.com>
2023-11-28 02:00:50 +0800

    fix: tooltip positioning

--- src/shared/components/tooltip/tooltip.tsx
+++ src/shared/components/tooltip/tooltip.tsx
@@ -14,18 +14,18 @@ const Tooltip: FC<TooltipProps> = (props) => {
   const { message, position, children } = props;
   return (
     <div className="group relative flex">
-      {children}
       <span
         className={classNames(
-          'absolute scale-0 whitespace-nowrap rounded bg-dark-primary p-2 text-xs text-white transition-all group-hover:scale-100 dark:bg-white dark:text-dark-primary',
+          'absolute min-w-[4rem] scale-0 whitespace-nowrap rounded bg-dark-primary p-2 text-center text-xs text-white transition-all group-hover:scale-100 dark:bg-white dark:text-dark-primary',
           position === 'top' ? 'bottom-8' : '',
-          position === 'bottom' ? 'right-[-3rem] top-8' : '',
+          position === 'bottom' ? 'right-[-3rem] top-10' : '',
           position === 'left' ? 'right-8' : '',
           position === 'right' ? 'right-8' : '',
         )}
       >
         {message}
       </span>
+      {children}
     </div>
   );
 };
