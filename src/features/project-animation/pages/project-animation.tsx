32f1a60c7378b3d2664f4bbd886bce72921748ec
Daragh <daraghcoll-@hotmail.com>
2023-11-27 17:42:58 +0800

    refactor: move providers to project animation component, from main tsx

--- src/features/project-animation/pages/project-animation.tsx
+++ src/features/project-animation/pages/project-animation.tsx
@@ -1,3 +1,7 @@
+import { FC } from 'react';
+import { SelectedElementProvider } from '../state/selected-element';
+import { AnimationsProvider } from '../state/animations';
+import { CanvasProvider } from '../state/canvas';
 import { Timeline } from '../components/timeline/timeline';
 import { ElementList } from '../components/element-list/element-list';
 import { Canvas } from '../components/canvas/canvas';
@@ -5,32 +9,38 @@ import { StyleControls } from '../components/style-controls/style-controls';
 import { Layers } from '../components/layers/layers';
 import { AnimationConfig } from '../components/animation-config/animation-config';
 
-function ProjectAnimation() {
+const ProjectAnimation: FC = () => {
   return (
-    <main className="flex h-screen flex-col gap-4 bg-light-secondary p-4 text-dark-primary dark:bg-dark-primary dark:text-white">
-      <div className="flex h-2/3 min-h-0 flex-row gap-4">
-        <div className="basis-1/5 overflow-scroll rounded-md bg-white p-2 dark:bg-dark-secondary">
-          <ElementList />
-        </div>
-        <Canvas />
-        <div
-          id="controls"
-          className="w-[240px] min-w-[240px] basis-1/5 overflow-scroll rounded-md bg-white dark:bg-dark-secondary"
-        >
-          <StyleControls />
-        </div>
-      </div>
-      <div className="flex h-1/3 min-h-0 gap-4">
-        <div className="basis-1/4 overflow-x-hidden overflow-y-scroll rounded-md bg-white p-2 text-dark-primary dark:bg-dark-secondary dark:text-white">
-          <Layers />
-        </div>
-        <Timeline />
-        <div className="basis-1/4 overflow-scroll rounded-md bg-white p-2 text-dark-primary dark:bg-dark-secondary dark:text-white">
-          <AnimationConfig />
-        </div>
-      </div>
-    </main>
+    <SelectedElementProvider>
+      <AnimationsProvider>
+        <CanvasProvider>
+          <main className="flex h-screen flex-col gap-4 bg-light-secondary p-4 text-dark-primary dark:bg-dark-primary dark:text-white">
+            <div className="flex h-2/3 min-h-0 flex-row gap-4">
+              <div className="basis-1/5 overflow-scroll rounded-md bg-white p-2 dark:bg-dark-secondary">
+                <ElementList />
+              </div>
+              <Canvas />
+              <div
+                id="controls"
+                className="w-[240px] min-w-[240px] basis-1/5 overflow-scroll rounded-md bg-white dark:bg-dark-secondary"
+              >
+                <StyleControls />
+              </div>
+            </div>
+            <div className="flex h-1/3 min-h-0 gap-4">
+              <div className="basis-1/4 overflow-x-hidden overflow-y-scroll rounded-md bg-white p-2 text-dark-primary dark:bg-dark-secondary dark:text-white">
+                <Layers />
+              </div>
+              <Timeline />
+              <div className="basis-1/4 overflow-scroll rounded-md bg-white p-2 text-dark-primary dark:bg-dark-secondary dark:text-white">
+                <AnimationConfig />
+              </div>
+            </div>
+          </main>
+        </CanvasProvider>
+      </AnimationsProvider>
+    </SelectedElementProvider>
   );
-}
+};
 
 export { ProjectAnimation };
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { SelectedElementProvider } from '@/features/project-animation/state/selected-element.tsx';
-import { AnimationsProvider } from '@/features/project-animation/state/animations.tsx';
-import { CanvasProvider } from '@/features/project-animation/state/canvas.tsx';
 import { ProjectAnimation } from '@features/project-animation/pages/project-animation.tsx';
 
 const router = createBrowserRouter([
@@ -16,12 +13,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SelectedElementProvider>
-      <AnimationsProvider>
-        <CanvasProvider>
-          <RouterProvider router={router} />
-        </CanvasProvider>
-      </AnimationsProvider>
-    </SelectedElementProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
