c8274e98f332c859d39e1e4b0e58f7fec62f1cf5
Daragh <daraghcoll-@hotmail.com>
2023-11-28 01:21:34 +0800

    feat: add standard dialog component

---
+++ src/shared/components/standard-dialog/standard-dialog.cy.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable react-refresh/only-export-components */
+import { Fragment, useState } from 'react';
+import { StandardDialog } from './standard-dialog';
+
+const MockComponent = () => {
+  const [openDialog, setOpenDialog] = useState(false);
+
+  return (
+    <Fragment>
+      <button data-cy="mock_component_dialog_button_open" onClick={() => setOpenDialog(true)}>
+        Click
+      </button>
+
+      <StandardDialog
+        onDialogClose={setOpenDialog}
+        openDialog={openDialog}
+        title="standard dialog title"
+        children={<span>standard dialog content</span>}
+      />
+    </Fragment>
+  );
+};
+
+describe('Standard Dialog', () => {
+  it('should open dialog on click', () => {
+    cy.mount(<MockComponent />);
+
+    cy.get('[data-cy="mock_component_dialog_button_open"]').click();
+
+    cy.get('[data-cy="standard_dialog"]').should('be.visible');
+  });
+
+  it('should open dialog on click and show title and message', () => {
+    cy.mount(<MockComponent />);
+
+    cy.get('[data-cy="mock_component_dialog_button_open"]').click();
+
+    cy.get('[data-cy="standard_dialog_title"]').contains('standard dialog title');
+    cy.get('[data-cy="standard_dialog_content"]').contains('standard dialog content');
+  });
+
+  it('should close dialog on cancel click', () => {
+    cy.mount(<MockComponent />);
+
+    cy.get('[data-cy="mock_component_dialog_button_open"]').click();
+    cy.get('[data-cy="standard_dialog"]').should('be.visible');
+
+    cy.get('body').click();
+    cy.get('[data-cy="standard_dialog_title"]').should(($el) => {
+      const doesNotExist = $el.length == 0;
+      const isNotVisible = !$el.is('visible');
+      const doesNotExistOrIsNotVisible = doesNotExist || isNotVisible;
+
+      expect(doesNotExistOrIsNotVisible, 'does not exist or is not visible').to.be.true;
+    });
+  });
+});
---
+++ src/shared/components/standard-dialog/standard-dialog.tsx
@@ -0,0 +1,70 @@
+import { Dialog, Transition } from '@headlessui/react';
+import { Dispatch, FC, Fragment, ReactNode, SetStateAction, useEffect, useState } from 'react';
+
+interface StandardDialogProps {
+  openDialog: boolean;
+  onDialogClose: Dispatch<SetStateAction<boolean>>;
+  title: string;
+  children: ReactNode;
+}
+
+const StandardDialog: FC<StandardDialogProps> = (props) => {
+  const { openDialog, onDialogClose, title, children } = props;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const close = () => {
+    setIsOpen(false);
+    onDialogClose(false);
+  };
+
+  useEffect(() => {
+    setIsOpen(openDialog);
+  }, [openDialog]);
+
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={close} data-cy="standard_dialog">
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-light-secondary p-6 text-left align-middle text-dark-secondary shadow-sm transition-all dark:border-2 dark:border-white dark:bg-dark-secondary dark:text-white">
+                <Dialog.Title
+                  as="h3"
+                  className=" p-2 text-lg font-medium leading-6 tracking-wide text-dark-primary dark:text-white"
+                  data-cy="standard_dialog_title"
+                >
+                  {title}
+                </Dialog.Title>
+                <div className="mt-2 text-sm text-dark-primary dark:text-white" data-cy="standard_dialog_content">
+                  {children}
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
+
+export { StandardDialog };
