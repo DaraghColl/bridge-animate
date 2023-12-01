331d6a4362c627fd1e010f88663a9ef1f8a7e913
Daragh <daraghcoll-@hotmail.com>
2023-12-02 01:01:44 +0800

    feat: add space prop to button

--- src/shared/components/button/button.tsx
+++ src/shared/components/button/button.tsx
@@ -3,16 +3,18 @@ import { FC, ReactNode } from 'react';
 type Variant = 'filled' | 'outlined' | 'ghost';
 type Color = 'inherit' | 'accent' | 'dark-primary' | 'light-primary';
 type Rounded = '0' | 'sm' | 'md' | 'lg';
+type Space = 'none' | 'sm' | 'md' | 'lg';
 
 interface ButtonComponentProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant: Variant;
   color: Color;
   rounded?: Rounded;
+  space?: Space;
   onClick: () => void;
   children?: ReactNode;
 }
 
-const getDynamicClasses = (variant: Variant, color: Color, rounded: Rounded) => {
+const getDynamicClasses = (variant: Variant, color: Color, rounded: Rounded, space: Space) => {
   const filled_text_color_map: Record<Color, string> = {
     inherit: 'text-inherit',
     accent: 'text-white',
@@ -33,18 +35,25 @@ const getDynamicClasses = (variant: Variant, color: Color, rounded: Rounded) => 
     'light-primary': 'bg-light-primary border-2 border-light-primary',
   };
 
-  return `${varient_map[variant]} ${color_map[color]} rounded-${rounded}`;
+  const space_map: Record<Space, string> = {
+    none: 'px-0 py-0',
+    sm: 'px-1.5 py-1',
+    md: 'px-3 py-2',
+    lg: 'px-4.5 py-3',
+  };
+
+  return `${varient_map[variant]} ${color_map[color]} rounded-${rounded} ${space_map[space]}`;
 };
 
 const baseClasses = 'flex items-center gap-2 border-2 px-3 py-2 transition-all ease-in-out hover:scale-105 text-sm';
 
 const Button: FC<ButtonComponentProps> = (props) => {
-  const { variant = 'filled', color, rounded = 'sm', children, onClick, ...attributes } = props;
+  const { variant = 'filled', color, rounded = 'sm', space = 'md', children, onClick, ...attributes } = props;
 
   return (
     <button
       type="button"
-      className={`${baseClasses} ${getDynamicClasses(variant, color, rounded)}`}
+      className={`${baseClasses} ${getDynamicClasses(variant, color, rounded, space)}`}
       onClick={onClick}
       {...attributes}
     >
