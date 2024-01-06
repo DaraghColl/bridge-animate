8483577c6e4943f180c69f8d70b2c3b318b09dc8
daragh coll <daraghcoll@daraghs-MacBook-Air.local>
2024-01-06 17:22:01 +1100

    feat: add dialog with demo info

--- src/features/project-animation/pages/project-animation.tsx
+++ src/features/project-animation/pages/project-animation.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, Fragment, useState } from 'react';
 import { SelectedElementProvider } from '../state/selected-element/selected-element';
 import { AnimationsProvider } from '../state/animations/animations';
 import { CanvasProvider } from '../state/canvas/canvas';
@@ -8,35 +8,54 @@ import { Canvas } from '../components/canvas/canvas';
 import { StyleControls } from '../components/style-controls/style-controls';
 import { Layers } from '../components/layers/layers';
 import { AnimationConfig } from '../components/animation-config/animation-config';
+import { StandardDialog } from '@/shared/components/standard-dialog/standard-dialog';
+import { Button } from '@/shared/components/button/button';
 
 const ProjectAnimation: FC = () => {
+  const [showDemoDialog, setShowDemoDialog] = useState(true);
   return (
     <SelectedElementProvider>
       <AnimationsProvider>
         <CanvasProvider>
-          <main className="flex h-screen flex-col gap-4 bg-light-secondary p-4 text-dark-primary dark:bg-dark-primary dark:text-white">
-            <div className="flex h-2/3 min-h-0 flex-row gap-4">
-              <div className="basis-1/5 overflow-scroll rounded-md bg-white p-2 dark:bg-dark-secondary">
-                <ElementList />
+          <Fragment>
+            <StandardDialog
+              title="This is a demo version of the app"
+              openDialog={showDemoDialog}
+              onDialogClose={() => setShowDemoDialog(false)}
+            >
+              <div className="mb-4 flex flex-col gap-2 px-2">
+                <p>It has been laoded with an svg and some animations.</p>
+                <p>Use the play controls to play the animation, and the scrubber to move through the animation.</p>
+                <p>Select a keyframe in the timeline to show style options</p>
               </div>
-              <Canvas />
-              <div
-                id="controls"
-                className="w-[240px] min-w-[240px] basis-1/5 overflow-scroll rounded-md bg-white dark:bg-dark-secondary"
-              >
-                <StyleControls />
+              <Button variant="filled" color="accent" rounded="md" onClick={() => setShowDemoDialog(false)}>
+                Close
+              </Button>
+            </StandardDialog>
+            <main className="flex h-screen flex-col gap-4 bg-light-secondary p-4 text-dark-primary dark:bg-dark-primary dark:text-white">
+              <div className="flex h-2/3 min-h-0 flex-row gap-4">
+                <div className="basis-1/5 overflow-scroll rounded-md bg-white p-2 dark:bg-dark-secondary">
+                  <ElementList />
+                </div>
+                <Canvas />
+                <div
+                  id="controls"
+                  className="w-[240px] min-w-[240px] basis-1/5 overflow-scroll rounded-md bg-white dark:bg-dark-secondary"
+                >
+                  <StyleControls />
+                </div>
               </div>
-            </div>
-            <div className="flex h-1/3 min-h-0 gap-4">
-              <div className="basis-1/4 overflow-x-hidden overflow-y-scroll rounded-md bg-white p-2 text-dark-primary dark:bg-dark-secondary dark:text-white">
-                <Layers />
+              <div className="flex h-1/3 min-h-0 gap-4">
+                <div className="basis-1/4 overflow-x-hidden overflow-y-scroll rounded-md bg-white p-2 text-dark-primary dark:bg-dark-secondary dark:text-white">
+                  <Layers />
+                </div>
+                <Timeline />
+                <div className="basis-1/4 overflow-scroll rounded-md bg-white p-2 text-dark-primary dark:bg-dark-secondary dark:text-white">
+                  <AnimationConfig />
+                </div>
               </div>
-              <Timeline />
-              <div className="basis-1/4 overflow-scroll rounded-md bg-white p-2 text-dark-primary dark:bg-dark-secondary dark:text-white">
-                <AnimationConfig />
-              </div>
-            </div>
-          </main>
+            </main>
+          </Fragment>
         </CanvasProvider>
       </AnimationsProvider>
     </SelectedElementProvider>
